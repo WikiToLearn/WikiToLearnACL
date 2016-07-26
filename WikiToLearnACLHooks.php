c7035011025a5bbd89a7403badde155b1b112ed7
Riccardo Iaconelli <riccardo@kde.org>
2016-07-26 13:50:43 +0200

    finish off extension
    Signed-off-by: Riccardo Iaconelli <riccardo@kde.org>

--- WikiToLearnACLHooks.php
+++ WikiToLearnACLHooks.php
@@ -1,8 +1,30 @@
 <?php
-if ( !defined( 'MEDIAWIKI' ) ){
-  die( );
+
+if (!defined('MEDIAWIKI')) {
+    die();
 }
 
-class WikiToLearnACLHooks {
+class WikiToLearnACLHooks
+{
+    public static function deleteProtection(&$title, &$user, $action, &$result)
+    {
+        global $wgOnlyUserEditUserPage;
+        $lTitle = explode('/', $title->getText());
 
+        # If we're not deleting, or we are allowed to do everything, let's just
+        # not mind
+        if (!($action == 'delete') || $user->isAllowed('wtl_editalluserpages')) {
+            return true;
+        }
+
+        # If we are an unprivileged user, but we're editing our page, then it's OK...
+        if (($title->mNamespace == NS_USER) && ($user->getname() == $lTitle[0])) {
+            $result = true;
+            return true;
+        # In all other cases, prevent deletion
+        } else {
+            $result = false;
+            return false;
+        }
+    }
 }
